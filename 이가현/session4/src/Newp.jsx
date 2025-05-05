1db64c39c31057ed3212e99c952cebd5f3316b25
ghgahyun <08gahyun31@naver.com>
2025-05-05 23:14:37 +0900

    Newp.jsx New.css 수정

--- 이가현/session4/src/Newp.jsx
+++ 이가현/session4/src/Newp.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Newp.css";
 import newpro from "./images/newpro.png";
 import watchs from "./images/watchseries.png";
+import ipad from "./images/image 3.png";
+import mac from "./images/image 2.png";
 
 const New = () => {
   return (
@@ -30,6 +32,26 @@ const New = () => {
               <p className="second1">Apple Watch Series 10</p>
               <p className="second2">얇아진 두께. 더 커진 존재감.</p>
               <p className="second3">599,000부터</p>
+            </div>
+          </div>
+        </div>
+        <div className="airpad">
+          <div className="image3">
+            <img src={ipad} alt="iPad Air" />
+            <div className="text3">
+              <p className="third1">iPad Air</p>
+              <p className="third2">바람처럼 빠르게</p>
+              <p className="third3">949,000부터</p>
+            </div>
+          </div>
+        </div>
+        <div className="mac">
+          <div className="image4">
+            <img src={mac} alt="MacBook Air" />
+            <div className="text4">
+              <p className="fourth1">MacBook Air</p>
+              <p className="fourth2">Apple Intelligence</p>
+              <p className="fourth3">1,590,000부터</p>
             </div>
           </div>
         </div>
